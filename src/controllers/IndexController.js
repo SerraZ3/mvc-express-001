a18f5ee2e6a01088b13af61f502936b74c6af70b
SerraZ3 <serra.henrique3@gmail.com>
2022-06-14 21:53:57 -0300

    Adicionando condicionais e laços de repetição no ejs

--- src/controllers/IndexController.js
+++ src/controllers/IndexController.js
@@ -1,12 +1,55 @@
 const indexController = {
   home: (req, res) => {
-    return res.render("index", { title: "Home" });
+    return res.render("index", { title: "" });
   },
   products: (req, res) => {
-    return res.render("products", { title: "Produtos" });
+    const produtos = [
+      {
+        nome: "Camisa Legal",
+        preco: "R$ 29,99",
+        descricao: "Camisa muito top",
+      },
+      {
+        nome: "Short Legal",
+        preco: "R$ 49,99",
+        descricao: "Short muito top",
+      },
+      {
+        nome: "Calça Estilosa",
+        preco: "R$ 99,99",
+        descricao: "Calça da hora",
+      },
+      {
+        nome: "Bone Paloso",
+        preco: "R$ 99,99",
+        descricao: "Boné da hora",
+      },
+    ];
+    return res.render("products", { title: "Produtos", produtos: produtos });
   },
   login: (req, res) => {
     return res.render("login", { title: "Login" });
+  },
+  users: (req, res) => {
+    const users = [
+      {
+        nome: "Henrique",
+        sobrenome: "Serra",
+        idade: 22,
+      },
+      {
+        nome: "Roberto Carlos",
+        sobrenome: "Silva",
+        idade: 89,
+      },
+      {
+        nome: "Augusto",
+        sobrenome: "Souza",
+        idade: 33,
+      },
+    ];
+    // return res.render("users", { title: "Usuários", users: [] });
+    return res.render("users", { title: "Usuários", users });
   },
 };
 
--- src/routes/indexRoute.js
+++ src/routes/indexRoute.js
@@ -6,5 +6,6 @@ const indexController = require("../controllers/IndexController");
 router.get("/", indexController.home);
 router.get("/products", indexController.products);
 router.get("/login", indexController.login);
+router.get("/users", indexController.users);
 
 module.exports = router;
